ce244d9cfc8a1e4c487cf5671343a4fc845ad92d
gl1n <39052367+gl1n@users.noreply.github.com>
2025-05-02 16:44:23 +0800

    调整delay&增加显示sha缩写

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,10 +4,10 @@ return {
         opts = {
             current_line_blame = true, -- 开启行内 blame
             current_line_blame_opts = {
-                delay = 500,
+                delay = 200,
                 virt_text_pos = "eol", -- 行尾显示
             },
-            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+            current_line_blame_formatter = "<abbrev_sha>, <author>, <author_time:%Y-%m-%d> - <summary>",
         },
     },
 }
